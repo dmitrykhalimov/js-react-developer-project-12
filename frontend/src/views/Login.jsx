3a3c7ba93981697cb94ad0281cb4ff62a414706c
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-07-26 16:36:05 +0300

    Добавляет форму

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
 
-const Login = () => { 
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+
+import * as Yup from 'yup';
+
+const SignupForm = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Formik
+      initialValues={{ login: '', password: ''}}
 
-export default Login;
+      validationSchema={Yup.object({
+        login: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+        password: Yup.string().max(20, 'Must be 20 characters or less').required('Required'),
+
+      })}
+
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+
+      <Form>
+        <label htmlFor="login">First Name</label>
+        <Field name="login" type="text" />
+        <ErrorMessage name="login" />
+        <label htmlFor="password">Last Name</label>
+        <Field name="password" type="text" />
+        <ErrorMessage name="password" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default SignupForm;
